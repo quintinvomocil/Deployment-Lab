b14a288fce405508054720fa0ecabd597ffd8c41
Quintin Vomocil <quintinvomocil@gmail.com>
2022-07-20 14:21:25 -0600

    Try again

--- server/index.js
+++ server/index.js
@@ -4,11 +4,15 @@ const path = require('path');
 const app = express();
 
 
-const port = process.env.PORT || 8008
-
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, "../index.html"))
 })
+
+app.get('/css', (req, res) => {
+    res.sendFile(path.join(__dirname, "../styles.css"))
+})
+
+const port = process.env.PORT || 8008
 
 app.listen(port, () => {
     console.log(`listening on port ${port}`);
